f2cf61ac321cdcc4ad91c0f32208ca31afea60d3
Zain Khatri <140105193+zainkhatri@users.noreply.github.com>
2024-10-01 18:37:14 +0000

    Added new files hello, hello.c, and firstfolder file

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  char hello[] = "My name is Zain and this is my first CSE 29 lab!!"; 
+  char hello2[] = { 72, 101, 108, 108, 111, 33, 0 };
+
+  puts(hello);
+  puts(hello2);
+
+  return 0;
+}
